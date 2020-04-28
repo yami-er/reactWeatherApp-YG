1a662fcd4733e7eb46bc366954c9f381148e3e73
yami-er <queenyani@gmail.com>
2020-04-28 19:56:41 -0400

    Updates

---
+++ precache-manifest.fe4bfee624a1173b293736d9cc1a1777.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a945abada49bb20ae5a4b0dda14571",
+    "url": "/reactWeatherApp-YG/index.html"
+  },
+  {
+    "revision": "843d78b83cc9a882dd97",
+    "url": "/reactWeatherApp-YG/static/css/main.0027d91a.chunk.css"
+  },
+  {
+    "revision": "fdb238d41195414be147",
+    "url": "/reactWeatherApp-YG/static/js/2.678403bf.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/reactWeatherApp-YG/static/js/2.678403bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843d78b83cc9a882dd97",
+    "url": "/reactWeatherApp-YG/static/js/main.4da1fa68.chunk.js"
+  },
+  {
+    "revision": "3de70c79e6144b0cc717",
+    "url": "/reactWeatherApp-YG/static/js/runtime-main.6b790363.js"
+  },
+  {
+    "revision": "92b906420afe32a0b52bb45ccb4320b1",
+    "url": "/reactWeatherApp-YG/static/media/pic4-bg.92b90642.jpg"
+  }
+]);
